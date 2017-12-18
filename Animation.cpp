625a8f0e0d0f2eae309db0904d9c3a7d71c018e3
Shunsaku Endo <32933169+ShunsakuVirus@users.noreply.github.com>
2017-12-18 20:30:12 +0900

    Update Animation.cpp

--- Animation.cpp
+++ Animation.cpp
@@ -1,26 +1,11 @@
-﻿//
-// アプリ雛形
-//
-// VisualStudioでもコンパイルできるようにするため
-// ソースコードをUTF-8(BOM付き)で用意:D
-//
-
-// Frameworkの全てをインクルード
 #include "lib/framework.hpp"
 
-// アプリのウインドウサイズ
 enum Size {
   WIDTH  = 800,
   HEIGHT = 600
 };
 
-//
-// メインプログラム
-//
 int main() {
-  // アプリウインドウを生成
-  // GLFWとOpenGLの初期化
-  // TIPS:色々な型が依存しているので、最初に生成する事
   AppEnv env(Size::WIDTH, Size::HEIGHT);
   Texture image("res/miku.png");
   int count = 0;
